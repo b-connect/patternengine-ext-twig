258f3ee9ddca7ce4588e0c520bba1ce5148b2348
Erik Seifert <erik.seifert@b-connect.de>
2018-04-09 13:41:31 +0200

    Update

--- src/PatternLab/PatternEngine/Twig/PatternLabEnvironment.php
+++ src/PatternLab/PatternEngine/Twig/PatternLabEnvironment.php
@@ -9,24 +9,16 @@ class PatternLabEnvironment extends \Twig_Environment {
   public function __construct(\Twig_LoaderInterface $loader = null, $options = array()) {
     parent::__construct($loader, $options);
     $this->addTokenParser(new TwigDocTokenParser());
+    $this->addExtension(new \Twig_Extension_StringLoader);
   }
-
 
   public function compileSource($source, $name = null) {
-    /*
-     * code to replace '[%' with '{% trans %}' in $source
-     * comes here ...
-     */
-
     if (preg_match(self::REG_EX_COMMENT, $source->getCode())) {
-      $code = preg_replace(self::REG_EX_COMMENT, "{% twig_doc %}\n{% verbatim %}\n$1\n{% endverbatim %}\n{% endtwig_doc %}", $source->getCode());
+      $path = realpath(__DIR__ . "/../../../../template/twig-doc-output.twig" );
+      $content = \file_get_contents($path);
+      $code = preg_replace(self::REG_EX_COMMENT, "{% twig_doc %}\n{% verbatim %}\n$1\n{% endverbatim %}\n{% endtwig_doc %} " . $content, $source->getCode());
       $source = new \Twig_Source($code, $source->getName(), $source->getPath());
     }
-
-    return parent::compileSource($source, $name = null);
+    return parent::compileSource($source, $name);
   }
 }
-
-
-
-$t = '/(?:#\-\{\#\s*|\#\})\n?/s';
